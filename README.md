b673e8bcc2f481c1a89c5f61ceb82c786fc3ae40
nobody <nobody@localhost>
2019-12-04 20:48:08 -0500

    test ping

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ DevOps Bot for Diswcord
 
 ## Depedencies
 
+* apt-get install redis-server
+
+* npm install redis
 * npm install discord-akairo
 * npm install discord.js
 
+## Invite to Server
+
+* https://discordapp.com/oauth2/authorize?client_id=<bot-client-id>&scope=bot
---
+++ bot.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+var owner = fs.readFileSync('/opt/secrets/operations-robot-owner', 'utf8');
+const { AkairoClient } = require('discord-akairo');
+const client = new AkairoClient({
+    ownerID: owner.trim(),
+    prefix: '!',
+    commandDirectory: './commands/'
+}, {
+    disableEveryone: false
+});
+
+var secret = fs.readFileSync('/opt/secrets/operations-robot', 'utf8');
+
+client.login(secret.trim());
+
+client.on("ready", () => {
+	console.log("My Owner Is:" + client.ownerID);
+	
+    client.user.setPresence({
+        game: { 
+            name: 'DevOps',
+            type: ''
+        },
+        status: 'online'
+    })
+
+	client.users.get(client.ownerID).send("Operations Robot is online.");
+});
+
+
